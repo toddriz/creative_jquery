7513a3abd30d9122fba6cb50e9c3089f07a2d26d
Todd Risenmay <toddriz@gmail.com>
2017-10-04 13:27:20 -0600

    added missing semicolon

--- jqueryCreative.js
+++ jqueryCreative.js
@@ -3,7 +3,7 @@ const apiKey = "dcAHh31rc5SYqqFQZOH2O1H3zRjl7tv3hP2Nxwp5";
 $(document).ready(function () {
     let date = new Date("2017-10-03");
     date = new Date().toISOString().slice(0,10);
-    const url = `https://api.nasa.gov/planetary/apod?hd=true&api_key=${apiKey}`
+    const url = `https://api.nasa.gov/planetary/apod?hd=true&api_key=${apiKey}`;
     $.getJSON(url, function (data) {
         $("#nasa-image").attr("src", data.hdurl);
     }).done(function () { console.log('getJSON request succeeded!'); })
