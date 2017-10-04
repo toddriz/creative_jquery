cf99574e6b4e0055a53309459c3822b792950383
Andrew <andrewjt678@gmail.com>
2017-10-04 15:00:19 -0700

    poop

--- jqueryCreative.js
+++ jqueryCreative.js
@@ -2,19 +2,25 @@ const apiKey = "dcAHh31rc5SYqqFQZOH2O1H3zRjl7tv3hP2Nxwp5";
 // api url example https://api.nasa.gov/planetary/apod?api_key=dcAHh31rc5SYqqFQZOH2O1H3zRjl7tv3hP2Nxwp5
 $(document).ready(function () {
 
-    let url = "https://api.nasa.gov/planetary/apod?hd=true&api_key=" + apiKey;
+    let nasa_url = "https://api.nasa.gov/planetary/apod?hd=true&api_key=" + apiKey;
+	let userInput = "";
+	let recipes = [];
 
-    $.getJSON(url, function (data) {
+    $.getJSON(nasa_url, function (data) {
         $("#nasa-image").attr("src", data.hdurl);
-    })
-        .done(function () {
-            console.log('getJSON request succeeded!');
-        })
-        .fail(function (jqXHR, textStatus, errorThrown) {
-            console.log('getJSON request failed! ' + textStatus);
-            console.log("incoming " + jqXHR.responseText);
-        })
-        .always(function () {
-            console.log('getJSON request ended!');
-        });
+    });
+     
+	 
+	$("#foodButton").click( (e) => {
+		let food_url = "https://api.edamam.com/search?q=" + $("#food").val() + "&app_id=dd148576&app_key=4809208db76ce80ead19002f527a10ff";
+		
+		$.getJSON(food_url, (data) =>{
+			recipes = data["hits"];
+			recipe = recipes[0]["recipe"];
+			$("#foodPic").attr("src", recipe["image"]);
+			$("#foodName").text(recipe["label"]);
+			$("#foodLink").text(recipe["url"]);
+			$("#foodLink").attr("href", recipe["url"]);
+		});
+	});
 });
